8d034374f69693ff89c4d6d21e060fd54b7e52d2
pedroqwertyu <pedro.h.m.f30@gmail.com>
2022-11-17 21:45:59 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [gato, setGato] = useState(null)
+
+  useEffect(() => {
+    getImagem()
+  }, [])
+
+  function fazGet(url) {
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.response
+  }
+
+  function getImagem() {
+    const data = fazGet("https://api.thecatapi.com/v1/images/search")
+    const array = JSON.parse(data)
+    const objeto = array[0]
+    setGato(objeto)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <img src={gato?.url} alt="logo" />
+      </div>
+      <div>
+        <button onClick={() => window.location.reload()}>RECARREGAR</button>
+      </div>
     </div>
   );
 }
